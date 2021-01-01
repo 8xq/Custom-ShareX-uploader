d2b060debb042392d5d281eb188d5f337f6a0774
HDzzzz <40918889+HDzzzz@users.noreply.github.com>
2021-01-01 04:55:20 +0000

    Update Config.php

--- Config.php
+++ Config.php
@@ -1,5 +1,4 @@
 <?php
-$domain = "http://localhost/shareX/"; // Replace this with your domain
 $secretKey = "MAKETHISRANDOM"; // Create a secrey key (make this very random)
 $domain = "Add IP / domain here (same as php script)"; // Replace this with your domain
 $directory = "Uploads/"; // Replace this with the directory where you would like to save images
