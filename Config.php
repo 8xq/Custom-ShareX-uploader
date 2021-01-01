25de701c4f20c0cf7fd39979c071b84ee84c8651
HDzzzz <40918889+HDzzzz@users.noreply.github.com>
2021-01-01 04:44:53 +0000

    Create Config.php

---
+++ Config.php
@@ -0,0 +1,7 @@
+<?php
+$domain = "http://localhost/shareX/"; // Replace this with your domain
+$secretKey = "MAKETHISRANDOM"; // Create a secrey key (make this very random)
+$domain = "Add IP / domain here (same as php script)"; // Replace this with your domain
+$directory = "Uploads/"; // Replace this with the directory where you would like to save images
+$ShareXheaderKey = 'MAKETHISRANDOM'; // This is another key that is checked so make sure this is random
+?>
