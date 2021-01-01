d6396e6e4b1118a59d02e6ceda876a9c23c1f3b2
HDzzzz <40918889+HDzzzz@users.noreply.github.com>
2021-01-01 04:45:40 +0000

    Update Config.php

--- Config.php
+++ Config.php
@@ -4,4 +4,11 @@ $secretKey = "MAKETHISRANDOM"; // Create a secrey key (make this very random)
 $domain = "Add IP / domain here (same as php script)"; // Replace this with your domain
 $directory = "Uploads/"; // Replace this with the directory where you would like to save images
 $ShareXheaderKey = 'MAKETHISRANDOM'; // This is another key that is checked so make sure this is random
+$supportedFiles = array( // this is a very simple array of allowed file upload types (by extension)
+  'gif',
+  'jpg',
+  'jpeg',
+  'png',
+  'bmp'
+);
 ?>
